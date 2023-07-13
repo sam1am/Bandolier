0e040f1e3a7f432a95b54d72eeb236dc4c3e6b61
sam1am <127555+sam1am@users.noreply.github.com>
2023-07-13 16:28:59 -0600

    ui
    Update the UI to look... different?

--- tools/assamilator/main.py
+++ tools/assamilator/main.py
@@ -9,9 +9,10 @@ from dotenv import load_dotenv
 import json
 import platform
 import tempfile
-import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
+from tkinter import messagebox
+import tkinter as tk
 
 load_dotenv()
 
@@ -31,27 +32,54 @@ def save_config(config):
     with open(CONFIG_FILE, 'w') as f:
         json.dump(config, f)
 
-config = load_config()
-folder_list = config.get('folder_list', [])
-print("Loaded config: ", config)
-VAULT_DIR = config.get('vault_dir', VAULT_DIR)
-
-root = tk.Tk()
-
-text_box = tk.Text(root, width=50, height=10)
-folder_listbox = tk.Listbox(root, width=50, height=10)
-
-# populate the listbox with the saved folders
-for folder in folder_list:
-    folder_listbox.insert(tk.END, folder)
-    # folder_list.insert(tk.END, folder)
-
-
 def detect_platform():
     if platform.system() == 'Darwin':
         return 'int8'
     else:
         return 'float16'
+
+config = load_config()
+folder_list = config.get('folder_list', [])
+print("Loaded config: ", config)
+VAULT_DIR = config.get('vault_dir', VAULT_DIR)
+
+compute_type = detect_platform()
+
+root = tk.Tk()
+root.title("Awesome Transcriber")
+root.geometry("600x400")
+
+style = ttk.Style()
+style.theme_use('clam')  # A theme that can be styled to be somewhat similar to Material Design
+
+style.configure('TButton', font=('Helvetica', 10), background='lightgrey')
+style.configure('TLabel', font=('Helvetica', 12), background='white')
+style.configure('TFrame', background='white')
+
+frame1 = ttk.Frame(root)
+frame1.pack(fill='x')
+
+frame2 = ttk.Frame(root)
+frame2.pack(fill='x')
+
+frame3 = ttk.Frame(root)
+frame3.pack(fill='both', expand=True)
+
+frame4 = ttk.Frame(root)
+frame4.pack(fill='x')
+
+vault_label = ttk.Label(frame1, text="Vault: " + VAULT_DIR)
+compute_type_label = ttk.Label(frame1, text="Compute Type: " + compute_type)  # New label to display compute type
+folder_listbox = tk.Listbox(frame2, width=50, height=10)
+vault_label.pack(side='left')
+compute_type_label.pack(side='left')  # Pack the new label
+
+for folder in folder_list:
+    folder_listbox.insert(tk.END, folder)
+
+text_box = tk.Text(frame3)
+text_box.configure(state='disabled') # Make the text box uneditable
+
 
 def copy_files(src_path, dst_path):
     files = os.listdir(src_path)
@@ -118,11 +146,11 @@ def set_vault():
     global VAULT_DIR
     VAULT_DIR = filedialog.askdirectory()
     save_config({'folder_list': folder_list, 'vault_dir': VAULT_DIR})
+    vault_label.config(text="Vault: "+VAULT_DIR)
 
 
 def start_processing():
     text_box.insert(tk.END, "Starting Process...")
-    compute_type = detect_platform()
     for folder in folder_list:
         if os.path.exists(folder):
             copy_files(folder, WORKING_DIR)
@@ -139,14 +167,15 @@ def start_processing():
             text_box.insert(tk.END, f"Folder {folder} not found.")
     text_box.insert(tk.END, "Finished Processing.")
 
-add_button = tk.Button(root, text="Add Folder", command=add_folder)
-set_button = tk.Button(root, text="Set Vault", command=set_vault)
-start_button = tk.Button(root, text="Start", command=start_processing)
+add_button = ttk.Button(frame4, text="Add Folder", command=add_folder)
+set_button = ttk.Button(frame4, text="Set Vault", command=set_vault)
+start_button = ttk.Button(frame4, text="Start", command=start_processing)
 
-add_button.pack()
-set_button.pack()
-start_button.pack()
-folder_listbox.pack()
-text_box.pack()
+vault_label.pack(side='left')
+folder_listbox.pack(fill='both', expand=True)
+text_box.pack(fill='both', expand=True)
+add_button.pack(side='left')
+set_button.pack(side='left')
+start_button.pack(side='left')
 
 root.mainloop()
